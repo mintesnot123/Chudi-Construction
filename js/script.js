f52bcd700f118b12d85564cb97c1509be5a613b8
Mintesnot Yismaw <yismawmintesnot@gmail.com>
2022-01-25 01:18:17 -0800

    facebook messanger added to home page 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
+window.fbAsyncInit = function() {
+	FB.init({
+	  appId            : '912333495590130',
+	  autoLogAppEvents : true,
+	  xfbml            : true,
+	  version          : 'v2.11'
+	});
+  };
+(function(d, s, id){
+	 var js, fjs = d.getElementsByTagName(s)[0];
+	 if (d.getElementById(id)) {return;}
+	 js = d.createElement(s); js.id = id;
+	 js.src = "https://connect.facebook.net/en_US/sdk.js";
+	 fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 jQuery(function ($) {
 	'use strict';
 
 	/* ----------------------------------------------------------- */
 	/*  Fixed header
 	/* ----------------------------------------------------------- */
+	
 	$(window).on('scroll', function () {
 
 		// fixedHeader on scroll
